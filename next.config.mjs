d2d915a6dad2b76d8800e91f6cd78064f06f4fa6
collinensurit <134040183+collinensurit@users.noreply.github.com>
2025-02-25 20:14:15 -0700

    Add deployment configuration files

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    serverComponentsExternalPackages: [],
+  },
+};
+
+export default nextConfig;
